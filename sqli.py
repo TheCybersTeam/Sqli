a6f58d5fd8024ac29fc87d8c505371865b8ca5ac
The Cybers Team <roohbots@gmail.com>
2016-05-12 06:17:21 -0300

    Beta 0.0

---
+++ sqli.py
@@ -0,0 +1,14 @@
+import subprocess as sp
+sp.call('cls',shell=True)
+header="""
+____ _              ___      _                     _____                      
+/__   \ |__   ___    / __\   _| |__   ___ _ __ ___  /__   \___  __ _ _ __ ___  
+  / /\/ '_ \ / _ \  / / | | | | '_ \ / _ \ '__/ __|   / /\/ _ \/ _` | '_ ` _ \ 
+ / /  | | | |  __/ / /__| |_| | |_) |  __/ |  \__ \  / / |  __/ (_| | | | | | |
+ \/   |_| |_|\___| \____/\__, |_.__/ \___|_|  |___/  \/   \___|\__,_|_| |_| |_|
+                         |___/                                                 
+
+More: https://www.facebook.com/TheCybersTeam
+Fast and easy SQLi hack tool Beta 0.0
+"""
+print header
